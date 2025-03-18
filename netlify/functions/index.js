e5c3be98585c3b6fc78d15d30de2e35f2a4a0497
alcanta-dev <ict@alcanta.it>
2025-03-18 14:25:56 +0100

    Utilizzo di import() dinamico per node-fetch

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,43 +1,30 @@
-const fetch = require('node-fetch');
+let fetch;
+import('node-fetch').then(module => {
+  fetch = module.default;
+});
 
 exports.handler = async (event, context) => {
+  if (!fetch) {
+    await import('node-fetch').then(module => {
+      fetch = module.default;
+    });
+  }
+
   try {
-    // Verifica che la richiesta sia di tipo POST
-    if (event.httpMethod !== 'POST') {
-      return {
-        statusCode: 405,
-        body: JSON.stringify({ error: 'Metodo non consentito' }),
-      };
-    }
-
-    // Ottieni i dati inviati dal form HubSpot
-    const data = JSON.parse(event.body);
-
-    // Leggi il token dalla variabile d'ambiente
-    const token = process.env.AUTH_TOKEN; // Usa la variabile d'ambiente
-
-    // Invia i dati al webhook di NodeRED
-    const response = await fetch('https://mylab.alcanta.it/contatti-convegno', {
-      method: 'POST',
-      headers: {
+    // Logica della funzione
+    const response = await fetch('https://mylab.alcanta.t/contatti-convegno', { 
+      method: 'POST', headers: {
         'Content-Type': 'application/json',
-        'Authorization': token, // Inserisci il token nell'header
+        'Authorization': process.env.AUTH_TOKEN,
       },
-      body: JSON.stringify(data), // Invia i dati del form
+      body: JSON.stringify(data),
     });
 
-    // Verifica se la richiesta a NodeRED è andata a buon fine
-    if (!response.ok) {
-      throw new Error('Errore durante l\'invio dei dati a NodeRED');
-    }
-
-    // Risposta di successo
     return {
       statusCode: 200,
       body: JSON.stringify({ message: 'Dati inviati correttamente a NodeRED' }),
     };
   } catch (error) {
-    // Gestione degli errori
     return {
       statusCode: 500,
       body: JSON.stringify({ error: error.message }),
